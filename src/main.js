e4ab8118d5d126f6db433003cc71e81adc9fb8b7
slackboi <slkassinthehood@gmail.com>
2021-01-31 01:54:51 +0800

    init

--- src/main.js
+++ src/main.js
@@ -257,11 +257,7 @@ if (false) {
 
   var isRuntime = (typeof loadRuntime === 'function');
   if (isRuntime) {
-<<<<<<< HEAD
     require('https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/settings.js');
-=======
-    require('https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/settings');
->>>>>>> 82d0ff7218bd8f84588749e9dc7baf2cd5b52c3d
     require('https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/cocos2d-runtime.min.js');
     require('jsb-adapter/engine/index.js');
   } else {
