25307d3c2bc16746b5dc4d008094c8e24108baf5
slackboi <slkassinthehood@gmail.com>
2021-01-30 23:17:42 +0800

    init

--- src/main.js
+++ src/main.js
@@ -196,7 +196,7 @@ window.boot = function () {
   if (false) {
     BK.Script.loadlib();
   } else {
-    var bundledScript = 'https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/project.min.js?ran=' + Math.random().toString("32").substring(2);
+    var bundledScript = 'https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/project.js?ran=' + Math.random().toString("32").substring(2);
     if (jsList) {
       jsList = jsList.map(function (x) {
         return 'https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/' + x;
@@ -257,11 +257,11 @@ if (false) {
 
   var isRuntime = (typeof loadRuntime === 'function');
   if (isRuntime) {
-    require('https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/settings.min.js');
+    require('https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/settings.js');
     require('https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/cocos2d-runtime.min.js');
     require('jsb-adapter/engine/index.js');
   } else {
-    require('https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/settings.min.js');
+    require('https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/settings.js');
     require('https://cdn.jsdelivr.net/gh/Slackboi/wjkpintu/src/cocos2d-jsb.js');
     require('jsb-adapter/jsb-engine.js');
   }
